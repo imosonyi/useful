f6f1e1e23dd60213c06967e41921b57e50673717
István Mosonyi <mosonyi15p@yahoo.com>
2020-08-01 18:55:16 +0200

    Added useascmd command and replaced exporting scripts to symlinks in install

--- linux/install.sh
+++ linux/install.sh
@@ -4,7 +4,8 @@
 sudo apt-get install -y xclip
 
 # Install scripts
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # Location of this script
-SCRIPTS_HOME="$DIR/scripts"
-$SCRIPTS_HOME/exportpath $SCRIPTS_HOME
+sh scripts/useascmd scripts/useascmd useascmd
+for script in scripts/*; do
+    useascmd $script ${script##*/}
+done
 
